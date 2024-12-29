e17142eec28970f2d70cc95de9219c89e763e5e4
m74jji <pytaek1@gmail.com>
2024-12-29 22:15:41 +0100

    update 1

--- src/app/api/setUserCountryInCookies/route.ts
+++ src/app/api/setUserCountryInCookies/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
+    /*
     // Parse the incoming request body as JSON
     const body = await request.json();
     const { userCountry } = body;
@@ -21,6 +22,7 @@ export async function POST(request: Request) {
     });
 
     return response;
+  */
   } catch (error) {
     console.log(error);
     return new NextResponse("Couldn't save data", {
